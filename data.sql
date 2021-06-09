7e57d9b7c44c62e2f7cf2143b38b9d35ddfe58c1
Emile Cyimena <cyimena09@hotmail.com>
2021-06-09 22:45:49 +0200

    fill and dev database

--- data.sql
+++ data.sql
@@ -1,12 +1,72 @@
-
-INSERT INTO user(fkApartment, firstname, lastname, email, phone, gender, street, house_number, box_number, zip, city, country, password)
+-- Address
+INSERT INTO address(street, house_number, box_number, zip, city, country)
 VALUES
-(1, 'Emile', 'Cyimena', 'cyimena09@hotmail.com', '0484090853', 'M', 'Rue Cour Boisacq', '18', 'B', 'zip', 'Bierges', 'Belgique', 'test1234='),
-(1, 'Benoit', 'Vankoningsloo', 'benoit@hotmail.com', '0477213465', 'M', 'Rue du Benoit', '59', 'B', 'zip', 'Mont-Saint-Guibert', 'France', 'test1234='),
-(1, 'Amaury', 'Cyemezo', 'cyemezo@hotmail.com', '0499591245', 'M', 'Rue Cour Boisacq', '18', 'B', 'zip', 'Bierges', 'Belgique', 'test1234='),
-(1, 'Alice', 'Malaika', 'malaika@hotmail.com', '0476134465', 'F', 'Rue de la source', '77', 'B', 'zip', 'Grez-Doiceau', 'Belgique', 'test1234='),
-(1, 'Mike', 'Francois', 'mike@hotmail.com', '0488124678', 'M', 'Rue Cour Boisacq', '18', 'B', 'zip', 'Bierges', 'Belgique', 'test1234=')
+('Rue Cour Boisacq1', '18', 'B', '1301', 'Bierges', 'Belgique'),
+('Rue du Benoit2', '59', 'B', '1302', 'Mont-Saint-Guibert', 'France'),
+('Rue Cour Boisacq3', '18', 'B', '1304', 'Bierges', 'Belgique'),
+('Rue de la source4', '77', 'B', '1305', 'Grez-Doiceau', 'Belgique'),
+('Rue Cour Boisacq5', '18', 'B', '1306', 'Bierges', 'Belgique'),
+('Rue Cour Boisacq6', '18', 'B', '1301', 'Bierges', 'Belgique'),
+('Rue du Benoit7', '59', 'B', '1302', 'Mont-Saint-Guibert', 'France'),
+('Rue Cour Boisacq8', '18', 'B', '1304', 'Bierges', 'Belgique'),
+('Rue de la source9', '77', 'B', '1305', 'Grez-Doiceau', 'Belgique'),
+('Rue Cour Boisacq10', '18', 'B', '1306', 'Bierges', 'Belgique'),
+('Rue de la source11', '77', 'B', '1305', 'Grez-Doiceau', 'Belgique'),
+('Rue Cour Boisacq12', '18', 'B', '1306', 'Bierges', 'Belgique'),
+('Rue Cour Boisacq13', '18', 'B', '1306', 'Bierges', 'Belgique');
+
+-- User
+INSERT INTO user(firstname, lastname, email, phone, gender, role, password, fkAddress )
+VALUES
+('Emile', 'Cyimena', 'cyimena09@hotmail.com', '0484090853', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 1),
+('Benoit', 'Vankoningsloo', 'benoit@hotmail.com', '0477213465', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 2),
+('Amaury', 'Cyemezo', 'cyemezo@hotmail.com', '0499591245', 'M', 'SYNDIC', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 3),
+('Alice', 'Malaika', 'malaika@hotmail.com', '0476134465', 'F', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 4),
+('Mike', 'Francois', 'mike@hotmail.com', '0488124678', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 5),
+('Susi', 'Toupe', 'stoupe0@symantec.com', '6685851293', 'F', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 6),
+('Eveline', 'Joyner', 'ejoyner1@cloudflare.com', '7861614104', 'F', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 7),
+('Eba', 'Penquet', 'epenquet2@walmart.com', '7779636737', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 8),
+('Justina', 'Dearth', 'jdearth3@hc360.com', '5481337107', 'F', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 9),
+('Venus', 'Tolwood', 'vtolwood4@w3.org', '2896359988', 'F', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 10),
+('Napoleon', 'Jencey', 'njencey5@csmonitor.com', '5625961927', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 11),
+('Kain', 'Wrist', 'kwrist6@mayoclinic.com', '5729661592', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 12),
+('Vernor', 'Titchen', 'vtitchen7@moonfruit.com', '2342462343', 'M', 'TENANT', '$2y$10$AKWxJHelS825lfPhx4rAG.dI0TX7bvQgsPzyFzQOB576rKF.U/8y2', 13);
 
 
 
-INSERT INTO building(name) Values ('La Belle Etoile')
+-- Building
+INSERT INTO building(name) VALUES ('Building A');
+INSERT INTO building(name) VALUES ('Building B');
+INSERT INTO building(name) VALUES ('Building C');
+INSERT INTO building(name) VALUES ('Building D');
+
+-- apartment
+INSERT INTO apartment(name, fkBuilding, fkOwner, fkTenant)
+VALUES
+('Apartement A1', 1, 2, 1),
+('Apartement A2', 1, 2, 2),
+('Apartement A3', 1, 2, 3),
+('Apartement A4', 1, 2, 4),
+('Apartement B1', 2, 2, 5),
+('Apartement B2', 2, 2, 6),
+('Apartement B3', 2, 2, 7),
+('Apartement C1', 3, 2, 8),
+('Apartement C2', 3, 2, 9),
+('Apartement C3', 3, 2, 10),
+('Apartement D1', 4, 2, 11),
+('Apartement D2', 4, 2, 12),
+('Apartement D3', 4, 2, 13);
+
+-- Communication
+INSERT INTO communication(subject, message, date_creation, last_update, fkBuilding)
+VALUES
+('Sujet Building A', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 1),
+('Sujet Building A', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 1),
+('Sujet Building B', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 2),
+('Sujet Building C', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 3),
+('Sujet Building D', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 4),
+('Sujet Building E', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 5),
+('Sujet Building F', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 6),
+('Sujet Building A', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 1),
+('Sujet Building A', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 1),
+('Sujet Building A', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis eum molestiaetur?', '2021-05-24', '2021-05-24', 1);
--- views/user/user-menu.php
+++ views/user/user-menu.php
@@ -1,15 +1,40 @@
-<ul id="user-space-menu">
-    <li><a href="/auth/accountView">Votre compte</a></li>
-    <li><a href="/ticket">Voir mes tickets</a></li>
-    <li><a href="/ticket/createView">Créer un ticket</a></li>
-    <li><a href="/communication/createView">Nouvelle communication</a></li>
-    <li><a href="/communication">Liste des communications</a></li>
-    <!--    Immeubles-->
-    <li><a href="/building">Liste des immeubles</a></li>
-    <li><a href="/building/createView">Ajouter un immeuble</a></li>
-    <!--    Appartements-->
-    <li><a href="/apartment">Liste des appartement</a></li>
-    <li><a href="/apartment/createView">Ajouter un appartement</a></li>
-    <!--    Utilisateurs-->
-    <li><a href="/user">Liste des utilisateurs</a></li>
-</ul>
+<div id="user-space-menu">
+    <ul >
+        <li><a href="/auth/accountView">Votre compte</a></li>
+        <li><a href="/communication">Liste des communications</a></li>
+    </ul>
+    <!-- ROLE == TENANT -->
+    <?php if ($user->role == 'TENANT'): ?>
+        <!--TICKETS-->
+        <ul>
+            <li><a href="/ticket">Voir mes tickets</a></li>
+            <li><a href="/ticket/createView">Créer un ticket</a></li>
+        </ul>
+    <?php endif; ?>
+
+    <!-- ROLE == SYNDIC -->
+    <?php if ($user->role == 'SYNDIC'): ?>
+        <!--COMMUNICATION-->
+        <h3>Communications</h3>
+        <ul>
+            <li><a href="/communication/createView">Nouvelle communication</a></li>
+        </ul>
+        <!-- IMMEUBLES -->
+        <h3>Immeubles</h3>
+        <ul>
+            <li><a href="/building">Liste des immeubles</a></li>
+            <li><a href="/building/createView">Ajouter un immeuble</a></li>
+        </ul>
+        <!-- APPARTEMENTS -->
+        <h3>Appartements</h3>
+        <ul>
+            <li><a href="/apartment">Liste des appartements</a></li>
+            <li><a href="/apartment/createView">Ajouter un appartement</a></li>
+        </ul>
+        <!-- PROPRIETAIRES -->
+        <h3>Propriétaires</h3>
+        <ul>
+            <li><a href="/user">Liste des utilisateurs</a></li>
+        </ul>
+    <?php endif; ?>
+</div>
