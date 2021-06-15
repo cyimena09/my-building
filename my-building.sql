747739104a02887a5f2773bc604a26d836ae9aa6
Benoit <benoit.vankoningsloo@bvkl.be>
2021-06-15 18:20:37 +0200

    suppression ticket-user

--- my-building.sql
+++ my-building.sql
@@ -198,34 +198,6 @@ INSERT INTO `ticket` VALUES (1,'Ticket Building A','1','Lorem ipsum dolor sit am
 UNLOCK TABLES;
 
 --
--- Table structure for table `ticket_user`
---
-
-DROP TABLE IF EXISTS `ticket_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket_user` (
-  `idTicket_User` int(11) NOT NULL AUTO_INCREMENT,
-  `fkUser` int(11) DEFAULT NULL,
-  `fkTicket` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idTicket_User`),
-  KEY `ticket_user_FK` (`fkUser`),
-  KEY `ticket_user_FK_1` (`fkTicket`),
-  CONSTRAINT `ticket_user_FK` FOREIGN KEY (`fkUser`) REFERENCES `user` (`idUser`),
-  CONSTRAINT `ticket_user_FK_1` FOREIGN KEY (`fkTicket`) REFERENCES `ticket` (`idTicket`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket_user`
---
-
-LOCK TABLES `ticket_user` WRITE;
-/*!40000 ALTER TABLE `ticket_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user`
 --
 
@@ -281,4 +253,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-15 17:37:07
+-- Dump completed on 2021-06-15 18:18:43
